fd8994b00454eed9370f19f907da2a64b6c4e312
Christina Delpone <cdelpone@gmail.com>
2021-08-25 08:26:43 -0600

    Add create and edit methods

--- app/controllers/distilleries_controller.rb
+++ app/controllers/distilleries_controller.rb
@@ -1,6 +1,48 @@
 class DistilleriesController < ApplicationController
   def index
-    # @distilleries = Distillery.all
-    @distilleries = ['distillery 1', 'distillery 2']
+    @distilleries = Distillery.all
+  end
+
+  def new
+  end
+
+  def create
+    distillery = Distillery.new({
+      name: params[:distillery][:name],
+      scotland_location: params[:distillery][:scotland_location],
+      established: params[:distillery][:established],
+      updated_at: params[:distillery][:updated_at],
+      created_at: params[:distillery][:created_at]
+    })
+  distillery.save
+  redirect_to '/distilleries'
+    require "pry"; binding.pry
+  end
+
+  def show
+    @distillery = Distillery.find(params[:id])
+  end
+
+  def edit
+    @distillery = Distillery.find(params[:id])
+  end
+
+  def update
+  distillery = Distillery.find(params[:id])
+
+  distillery.update({
+    name: params[:distillery][:name],
+    scotland_location: params[:distillery][:scotland_location],
+    established: params[:distillery][:established],
+    updated_at: params[:distillery][:updated_at],
+    created_at: params[:distillery][:created_at]
+    })
+    distillery.save
+    redirect_to '/distilleries/#{distillery.id}'
+  end
+
+  def destroy
+    Distillery.destroy(params[:id])
+    redirect_to '/distilleries'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   get '/', to: 'welcome#index'
   get '/skateboards', to: 'skateboards#index'
   get '/distilleries', to: 'distilleries#index'
+  get '/distilleries/new', to: 'distilleries#new'
+  post '/distilleries', to: 'distilleries#create'
+  get '/distilleries/:id', to: 'distilleries#show'
+  get '/distilleries/:id/edit', to: 'distilleries#edit'
+  patch '/distilleries/:id', to: 'distilleries#update'
+  delete '/distilleries/:id', to: 'distilleries#destroy'
 end
