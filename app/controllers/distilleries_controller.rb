b8649f64905f3101ce276fec8ae8aa32cdabd3ba
Christina Delpone <cdelpone@gmail.com>
2021-08-25 11:53:57 -0600

    Update edit options and view'

--- app/controllers/distilleries_controller.rb
+++ app/controllers/distilleries_controller.rb
@@ -1,6 +1,10 @@
 class DistilleriesController < ApplicationController
   def index
     @distilleries = Distillery.all
+    # in the distilleryscotchescontroller
+    #     @distillery = Distillery.find(params[:id])
+    # for helper private helper method
+    #     @distillery_scotches = distillery_scotches
   end
 
   def new
@@ -14,9 +18,8 @@ class DistilleriesController < ApplicationController
       updated_at: params[:distillery][:updated_at],
       created_at: params[:distillery][:created_at]
     })
-  distillery.save
-  redirect_to '/distilleries'
-    require "pry"; binding.pry
+    distillery.save
+    redirect_to '/distilleries'
   end
 
   def show
@@ -28,14 +31,14 @@ class DistilleriesController < ApplicationController
   end
 
   def update
-  distillery = Distillery.find(params[:id])
+    distillery = Distillery.find(params[:id])
 
-  distillery.update({
-    name: params[:distillery][:name],
-    scotland_location: params[:distillery][:scotland_location],
-    established: params[:distillery][:established],
-    updated_at: params[:distillery][:updated_at],
-    created_at: params[:distillery][:created_at]
+    distillery.update({
+      name: params[:distillery][:name],
+      scotland_location: params[:distillery][:scotland_location],
+      established: params[:distillery][:established],
+      updated_at: params[:distillery][:updated_at],
+      created_at: params[:distillery][:created_at]
     })
     distillery.save
     redirect_to '/distilleries/#{distillery.id}'
