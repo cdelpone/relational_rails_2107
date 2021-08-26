2171d31c792b687b54681343160b93f14a5c0a93
Rteske <ryanteske@outlook.com>
2021-08-25 17:39:18 -0700

    Start feature testing

--- spec/features/brands/show_spec.rb
+++ spec/features/brands/show_spec.rb
@@ -1,34 +1,26 @@
 require "rails_helper"
 
-RSpec.describe 'can dispay an index with options' do
+RSpec.describe 'can dispay brand with its attributes' do
   before :each do
     @brand_1 = Brand.create!({
       brand_name: "dogtown",
       established: 2000,
       gnarly: true
       })
-    @brand_2 = Brand.create!({
-      brand_name: "Birdhouse",
-      established: 1999,
-      gnarly: false
-      })
 
-    visit('/brands')
+    visit("/brands/#{@brand_1.id}")
   end
 
   it 'can display brand names' do
     expect(page).to have_content(@brand_1.brand_name)
-    expect(page).to have_content(@brand_2.brand_name)
   end
 
   it 'can display years established' do
     expect(page).to have_content(@brand_1.established)
-    expect(page).to have_content(@brand_2.established)
   end
 
   it 'can display if the company is gnarly or not' do
     expect(page).to have_content(@brand_1.gnarly)
-    expect(page).to have_content(@brand_2.gnarly)
   end
 
 end
