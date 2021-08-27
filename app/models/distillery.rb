a5248b5cce4ceae4580ea88c5db54a8edccf681e
Christina Delpone <cdelpone@gmail.com>
2021-08-27 15:00:54 -0600

    test instance and class methods on parent model

--- app/models/distillery.rb
+++ app/models/distillery.rb
@@ -3,9 +3,11 @@ class Distillery < ApplicationRecord
 
   def self.order_by_creation
     self.all.order(created_at: :desc )
+    # class method - calling on multiple
   end
 
   def count_scotches
     scotches.count
+    # calling this method on an instance of a distillery
   end
 end
