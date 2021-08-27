52a6b6d7780f708e57e00357d609f3d64f701e29
Rteske <ryanteske@outlook.com>
2021-08-27 14:00:00 -0700

    Add edit feature test

---
+++ spec/features/brands/edit_spec.rb
@@ -0,0 +1,33 @@
+require "rails_helper"
+
+RSpec.describe 'can change an existing brand' do
+  before :each do
+    @brand = Brand.create!({
+      brand_name: "dogtown",
+      established: 2000,
+      gnarly: true
+      })
+
+    visit("/brands/#{@brand.id}/edit")
+  end
+
+  it 'can change existing brand' do
+    fill_in "brand[brand_name]", with: "Suckboy"
+    fill_in "brand[established]", with: "100"
+    fill_in "brand[gnarly]", with: "false"
+
+    expect(page).to have_button("Submit")
+
+    click_on "Submit"
+
+    expect(page).to have_content("Suckboy")
+    expect(page).to have_content("100")
+    expect(page).to have_content("false")
+  end
+
+  it 'redirects to the show page' do
+    click_on "Submit"
+
+    expect(current_path).to eq("/brands/#{@brand.id}")
+  end
+end
--- spec/features/brands/show_spec.rb
+++ spec/features/brands/show_spec.rb
@@ -2,25 +2,25 @@ require "rails_helper"
 
 RSpec.describe 'can dispay brand with its attributes' do
   before :each do
-    @brand_1 = Brand.create!({
+    @brand = Brand.create!({
       brand_name: "dogtown",
       established: 2000,
       gnarly: true
       })
 
-    visit("/brands/#{@brand_1.id}")
+    visit("/brands/#{@brand.id}")
   end
 
   it 'can display brand names' do
-    expect(page).to have_content(@brand_1.brand_name)
+    expect(page).to have_content(@brand.brand_name)
   end
 
   it 'can display years established' do
-    expect(page).to have_content(@brand_1.established)
+    expect(page).to have_content(@brand.established)
   end
 
   it 'can display if the company is gnarly or not' do
-    expect(page).to have_content(@brand_1.gnarly)
+    expect(page).to have_content(@brand.gnarly)
   end
 
 end
