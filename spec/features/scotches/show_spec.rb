c94104404352db172fa665e6fab66a46f150684c
Christina Delpone <cdelpone@gmail.com>
2021-08-27 15:42:00 -0600

    Test link to distillery and scotch indexes

--- spec/features/scotches/show_spec.rb
+++ spec/features/scotches/show_spec.rb
@@ -1,9 +1,5 @@
-# User Story 4, Child Show (x2)
-# As a visitor
-# When I visit '/child_table_name/:id'
-# Then I see the child with that id including the child's attributes:
 require 'rails_helper'
-
+# rspec spec/features/scotches/show_spec.rb
 RSpec.describe 'scotch show page', type: :feature do
   before :each do
     @distillery_1 = Distillery.create!({
@@ -17,6 +13,14 @@ RSpec.describe 'scotch show page', type: :feature do
                                     established: 1779,
                                     scotland_location: true,
                                     id: 7
+                                    })
+    @distillery_6 = Distillery.create!({
+                                    name: "Laphroaig",
+                                    established: 1815,
+                                    scotland_location: true,
+                                    updated_at: '2021-08-26 00:06:30 UTC',
+                                    created_at: '2021-08-26 00:06:30 UTC',
+                                    id: 6
                                     })
     @scotch_1 = Scotch.create!({
                                     name: "An OA",
@@ -48,7 +52,8 @@ RSpec.describe 'scotch show page', type: :feature do
   end
 
   describe 'only displays the name and attributes of the scotch selected' do
-
+    # User Story 4, Child Show (x2)
+    # As a visitor; # When I visit '/child_table_name/:id'; # Then I see the child with that id including the child's attributes:
     it 'only displays the name of the scotch selected' do
       visit '/scotches/1'
       expect(page).to have_content(@scotch_1.name)
@@ -92,6 +97,41 @@ RSpec.describe 'scotch show page', type: :feature do
       visit '/scotches/3'
       expect(page).to have_content(@scotch_3.updated_at)
       expect(page).to have_no_content(@scotch_2.updated_at)
+    end
+
+    # it 'only displays the distillery id associated with the scotch selected' do
+    #   visit '/scotches/3'
+    #   expect(page).to have_content(@scotch_3.distillery_id)
+    #   expect(page).to have_no_content(@scotch_2.distillery_id)
+    #   expect(page).to have_no_content(@scotch_1.distillery_id)
+    #   visit '/scotches/1'
+    #   expect(page).to have_content(@scotch_1.distillery_id)
+    #   expect(page).to have_no_content(@scotch_2.distillery_id)
+    #   expect(page).to have_no_content(@scotch_3.distillery_id)
+    # end
+  end
+
+  describe 'does other things on the show page for the scotch selected' do
+    it 'links to the distillery index page(interactivity)' do
+      # User Story 9, Parent Index Link
+      # As a visitor; # When I visit any page on the site; # Then I see a link at the top of the page that takes me to the Parent Index
+      visit '/distilleries/7'
+      expect(page).to have_link('Distilleries')
+      click_link 'Distilleries'
+      expect(current_path).to eq("/distilleries")
+      expect(page).to have_content(@distillery_6.name)
+    end
+
+    it 'links to the scotches index page(interactivity)' do
+      # User Story 9, Parent Index Link
+      # As a visitor; # When I visit any page on the site; # Then I see a link at the top of the page that takes me to the Parent Index
+      visit '/distilleries/7'
+      expect(page).to have_link('Scotches')
+      click_link 'Scotches'
+      expect(current_path).to eq("/scotches")
+      expect(page).to have_content(@scotch_1.name)
+      expect(page).to have_content(@scotch_2.name)
+      expect(page).to have_content(@scotch_3.name)
     end
   end
 end
