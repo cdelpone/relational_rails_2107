b57b4e8cb1639f6be608adf1020781c35c7ad8b9
Christina Delpone <cdelpone@gmail.com>
2021-08-27 15:22:12 -0600

    Test link to distillery and scotch indexes at top of page

--- spec/features/distilleries/index_spec.rb
+++ spec/features/distilleries/index_spec.rb
@@ -1,8 +1,3 @@
-# User Story 1, Parent Index (x2)
-# For each parent table
-# As a visitor
-# When I visit '/parents'
-# Then I see the name of each parent record in the system
 require 'rails_helper'
 # rspec spec/features/distilleries/index_spec.rb
 RSpec.describe 'distilleries index page', type: :feature do
@@ -35,17 +30,23 @@ RSpec.describe 'distilleries index page', type: :feature do
   end
 
   it 'can see the names of all distilleries' do
+    # User Story 1, Parent Index (x2)
+    # For each parent table; # As a visitor; # When I visit '/parents'; # Then I see the name of each parent record in the system
     expect(page).to have_content(@distillery_6.name)
     expect(page).to have_content(@distillery_1.name)
     expect(page).to have_content(@distillery_7.name)
   end
 
   it 'displays records in order of creation (most recently created first)' do
+    # User Story 6, Parent Index sorted by Most Recently Created (x2)
+    # As a visitor; # When I visit the parent index,; # I see that records are ordered by most recently created first; # And next to each of the records I see when it was created
     expect("Laphroaig").to appear_before("Ardbeg", only_text: true)
     expect("Bowmore").to appear_before("Laphroaig", only_text: true)
   end
 
   it 'displays the date the record was created for each distillery' do
+    # User Story 6, Parent Index sorted by Most Recently Created (x2)
+    # As a visitor; # When I visit the parent index,; # I see that records are ordered by most recently created first; # And next to each of the records I see when it was created
     expect("2021-08-26 01:11:07 UTC").to appear_before("Laphroaig", only_text: true)
     expect("2021-08-26 00:06:30 UTC").to appear_before("Ardbeg", only_text: true)
     expect("2021-08-25 16:34:12 UTC").to_not appear_before("Laphroaig", only_text: true)
@@ -61,4 +62,19 @@ RSpec.describe 'distilleries index page', type: :feature do
     expect(page).to have_content(@distillery_6.name)
   end
 
+  it 'shows a link to the distillery index page' do
+    # User Story 9, Parent Index Link
+    # As a visitor; # When I visit any page on the site; # Then I see a link at the top of the page that takes me to the Parent Index
+    expect(page).to have_link('Distilleries')
+    click_link 'Distilleries'
+    expect(current_path).to eq("/distilleries")
+  end
+
+  it 'shows a link to the scotches index page' do
+    # User Story 9, Parent Index Link
+    # As a visitor; # When I visit any page on the site; # Then I see a link at the top of the page that takes me to the Parent Index
+    expect(page).to have_link('Scotches')
+    click_link 'Scotches'
+    expect(current_path).to eq("/scotches")
+  end
 end
