c1f562b3fb017f4d74d6c6ac84bcb7946ffcb869
Christina Delpone <cdelpone@gmail.com>
2021-08-27 12:16:11 -0600

    Add validation test, describe block for instance methods

--- spec/models/distillery_spec.rb
+++ spec/models/distillery_spec.rb
@@ -3,5 +3,9 @@ require 'rails_helper'
 describe Distillery, type: :model do
   describe 'validations' do
     it { should have_many(:scotches) }
+    # it { should validate_presence_of(:name) }
+  end
+
+  describe 'instance methods' do
   end
 end
