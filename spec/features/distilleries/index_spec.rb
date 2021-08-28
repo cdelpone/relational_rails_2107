dd553336ea5923e9309f284cb77e1a7d43c4cfb5
Christina Delpone <cdelpone@gmail.com>
2021-08-28 15:19:05 -0600

    Test link to create new distillery page

--- spec/features/distilleries/index_spec.rb
+++ spec/features/distilleries/index_spec.rb
@@ -77,4 +77,12 @@ RSpec.describe 'distilleries index page', type: :feature do
     click_link 'Scotches'
     expect(current_path).to eq("/scotches")
   end
+
+  it 'shows a link to create a new distillery' do
+    # User Story 11, Parent Creation (x2)
+    # As a visitor; # When I visit the Parent Index page; # Then I see a link to create a new Parent record, "New Parent"; # When I click this link; # Then I am taken to '/parents/new' where I  see a form for a new parent record; # When I fill out the form with a new parent's attributes:; # And I click the button "Create Parent" to submit the form; # Then a `POST` request is sent to the '/parents' route,; # a new parent record is created,; # and I am redirected to the Parent Index page where I see the new Parent displayed.
+    expect(page).to have_link('New Distillery')
+    click_link 'New Distillery'
+    expect(current_path).to eq("/distilleries/new")
+  end
 end
