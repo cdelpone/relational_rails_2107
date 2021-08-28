43a2b0d400168b6d07309ed1b643d74f3c3f60cf
Christina Delpone <cdelpone@gmail.com>
2021-08-28 16:56:55 -0600

    Change update/edit link to Update

--- spec/features/distilleries/show_spec.rb
+++ spec/features/distilleries/show_spec.rb
@@ -148,8 +148,8 @@ RSpec.describe 'distillery show page', type: :feature do
     # User Story 12, Parent Update (x2)
     # As a visitor; # When I visit a parent show page; # Then I see a link to update the parent "Update Parent"; # When I click the link "Update Parent"; # Then I am taken to '/parents/:id/edit' where I  see a form to edit the parent's attributes:; # When I fill out the form with updated information; # And I click the button to submit the form; # Then a `PATCH` request is sent to '/parents/:id',; # the parent's info is updated,; # and I am redirected to the Parent's Show page where I see the parent's updated info
       visit '/distilleries/7'
-      expect(page).to have_link('Update/Edit')
-      click_link 'Update/Edit'
+      expect(page).to have_link('Update')
+      click_link 'Update'
       expect(current_path).to eq('/distilleries/7/edit')
     end
   end
