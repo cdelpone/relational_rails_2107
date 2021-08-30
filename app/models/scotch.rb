8fb434eceb48b804166b398459b8c983ba13cf29
Christina Delpone <cdelpone@gmail.com>
2021-08-30 14:15:29 -0600

    Test validations and relationships

--- app/models/scotch.rb
+++ app/models/scotch.rb
@@ -1,3 +1,12 @@
 class Scotch < ApplicationRecord
+  validates_presence_of :name, :single_malt, :year
+
   belongs_to :distillery
+
+# if params[:order]
+#   chara.order(:name)
+# else chars
+#   in p/c contrllers
+#   if params [:sort]
+
 end
