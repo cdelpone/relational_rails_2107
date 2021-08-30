6b01abbf6f57bcf6e7d321e1e922873b03ef11fe
Christina Delpone <cdelpone@gmail.com>
2021-08-30 11:15:09 -0600

    Test distillery has a new page that creates new record / redirects to show

--- spec/features/distilleries/new_spec.rb
+++ spec/features/distilleries/new_spec.rb
@@ -16,21 +16,22 @@ RSpec.describe 'new distillery page', type: :feature do
     # As a visitor; # When I visit the Parent Index page; # Then I see a link to create a new Parent record, "New Parent"; # When I click this link; # Then I am taken to '/parents/new' where I  see a form for a new parent record; # When I fill out the form with a new parent's attributes:; # And I click the button "Create Parent" to submit the form; # Then a `POST` request is sent to the '/parents' route,; # a new parent record is created,; # and I am redirected to the Parent Index page where I see the new Parent displayed.
     # expect(page).to have_select 'distillery[scotland_location]', options: ['true', 'false']
     fill_in 'distillery[name]', with: "Highland Park Distillery"
-    choose 'true'
+    choose 'false'
     fill_in 'distillery[established]', with: '1798'
     expect(page).to have_button('Create Distillery')
+    # require "pry"; binding.pry
   end
 
-  it 'retirects to distilleries index and shows new disillery' do
+  it 'redirects to distilleries index and shows new disillery' do
     # User Story 11, Parent Creation (x2)
     # As a visitor; # When I visit the Parent Index page; # Then I see a link to create a new Parent record, "New Parent"; # When I click this link; # Then I am taken to '/parents/new' where I  see a form for a new parent record; # When I fill out the form with a new parent's attributes:; # And I click the button "Create Parent" to submit the form; # Then a `POST` request is sent to the '/parents' route,; # a new parent record is created,; # and I am redirected to the Parent Index page where I see the new Parent displayed.
     fill_in 'distillery[name]', with: "Highland Park Distillery"
-    choose 'true'
+    choose 'false'
     fill_in 'distillery[established]', with: '1798'
     click_button 'Create Distillery'
     expect(current_path).to eq('/distilleries')
     expect(page).to have_content('Highland Park')
-    expect(page).to have_content('true')
+    expect(page).to have_content('false')
     expect(page).to have_content('1798')
   end
   # before { get :new }
