ea33a795a3365020fa9d5a48b725fc57173a5a8c
Christina Delpone <cdelpone@gmail.com>
2021-08-31 17:49:36 -0600

    Test new scotch is created with scotland location and single malt as false

--- spec/features/distilleries/scotches/index_spec.rb
+++ spec/features/distilleries/scotches/index_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe 'Distillery scotches index' do
     @distillery_7 = Distillery.create!({
                                     name: "Bowmore",
                                     established: 1779,
-                                    scotland_location: true,
+                                    scotland_location: false,
                                     id: 7
                                     })
     @distillery_6 = Distillery.create!({
@@ -33,12 +33,20 @@ RSpec.describe 'Distillery scotches index' do
                                     })
     @scotch_2 = Scotch.create!({
                                     name: "Bowmore 10 yr",
-                                    single_malt: false,
+                                    single_malt: true,
                                     year: 10,
                                     updated_at: '2021-08-26 20:10:37 UTC',
                                     created_at: '2021-08-26 20:10:37 UTC',
                                     distillery_id: 7,
                                     id: 2
+                                    })
+    @scotch_14 = @distillery_7.scotches.create!({
+                                    name: "Bowmore 14 yr",
+                                    single_malt: false,
+                                    year: 12,
+                                    updated_at: '2021-08-26 21:34:25 UTC',
+                                    created_at: '2021-08-26 21:34:25 UTC',
+                                    id: 14
                                     })
     @scotch_3 = Scotch.create!({
                                     name: "Bowmore 12 yr",
@@ -65,6 +73,7 @@ RSpec.describe 'Distillery scotches index' do
     # User Story 5, Parent Children Index (x2)
     # As a visitor; # When I visit '/parents/:parent_id/child_table_name'; # Then I see each Child that is associated with that Parent with each Child's attributes:
     expect(page).to have_content(@scotch_2.name)
+    expect(page).to have_content(@scotch_14.name)
     expect(page).to have_content(@scotch_2.single_malt)
     expect(page).to have_content(@scotch_2.year)
     expect(page).to have_content(@scotch_2.distillery_id)
@@ -77,7 +86,9 @@ RSpec.describe 'Distillery scotches index' do
     # User Story 9, Parent Index Link
     # As a visitor; # When I visit any page on the site; # Then I see a link at the top of the page that takes me to the Parent Index
     expect(page).to have_link('Distilleries')
+
     click_link 'Distilleries'
+
     expect(current_path).to eq("/distilleries")
     expect(page).to have_content(@distillery_6.name)
   end
@@ -85,7 +96,9 @@ RSpec.describe 'Distillery scotches index' do
   it 'links to the scotches index page(interactivity)' do
     # User Story 8, Child Index Link; # As a visitor; # When I visit any page on the site; # Then I see a link at the top of the page that takes me to the Child Index
     expect(page).to have_link('Scotches')
+
     click_link 'Scotches'
+
     expect(current_path).to eq("/scotches")
     expect(page).to have_content(@scotch_1.name)
     expect(page).to have_content(@scotch_2.name)
@@ -95,19 +108,24 @@ RSpec.describe 'Distillery scotches index' do
   it 'has a link to create new scotch for this distillery' do
     # User Story 13, Parent Child Creation (x2); # As a visitor; # When I visit a Parent Childs Index page; # Then I see a link to add a new adoptable child for that parent "Create Child"; # When I click the link; # I am taken to '/parents/:parent_id/child_table_name/new' where I see a form to add a new adoptable child; # When I fill in the form with the child's attributes; # And I click the button "Create Child"; # Then a `POST` request is sent to '/parents/:parent_id/child_table_name',; # a new child object/row is created for that parent,; # and I am redirected to the Parent Childs Index page where I can see the new child listed;
     expect(page).to have_link('Create Scotch')
+
     click_link 'Create Scotch'
+
     expect(current_path).to eq("/distilleries/7/scotches/new")
     expect(page).to have_content('New Scotch')
+
     fill_in "Scotch Name:", with: 'Bowmore 15 yr'
-    check('Single Malt?')
-    check('Yes')
     fill_in 'Year:', with: '15'
 
     expect(page).to have_button('Create Scotch')
+
     click_button 'Create Scotch'
+    save_and_open_page
+
     expect(current_path).to eq("/distilleries/#{@distillery_7.id}/scotches")
     expect(page).to have_content('Bowmore 15 yr')
     expect(page).to have_content('15')
+    expect(page).to have_content('false')
   end
 
   it 'links to a an alphabetically sorted list of scotches' do
@@ -124,16 +142,18 @@ RSpec.describe 'Distillery scotches index' do
                                     })
     scotch_9 = Scotch.create!({
                                     name: "A Cool Scotch 2 yr",
-                                    single_malt: true,
+                                    single_malt: false,
                                     year: 2,
                                     updated_at: '2021-08-26 21:34:25 UTC',
                                     created_at: '2021-08-26 21:34:25 UTC',
                                     distillery_id: 7,
                                     id: 9
                                     })
-    expect("Cool Scotch 20 yr").to appear_before("A Cool Scotch 2 yr", only_text: true)
+
     expect(page).to have_link('Sort Alphabetically')
+
     click_link 'Sort Alphabetically'
+
     expect(current_path).to eq("/distilleries/7/scotches")
     expect("A Cool Scotch 2 yr").to appear_before("Cool Scotch 20 yr", only_text: true)
     expect("A Cool Scotch 2 yr").to appear_before("Bowmore 5 yr", only_text: true)
