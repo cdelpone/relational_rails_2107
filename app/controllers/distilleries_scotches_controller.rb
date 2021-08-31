d3d356729c57a5ca1e1a718888c763cf8cb43501
Christina Delpone <cdelpone@gmail.com>
2021-08-30 18:04:35 -0600

    Test creates new distillery/scotch, renders sorted alpha

--- app/controllers/distilleries_scotches_controller.rb
+++ app/controllers/distilleries_scotches_controller.rb
@@ -1,29 +1,25 @@
 class DistilleriesScotchesController < ApplicationController
   def index
     @distillery = Distillery.find(params[:distillery_id])
-    @scotches = @distillery.scotches
-      after_action
+    @scotches = if params[:order] = 'alphabetical'
+      @distillery.sort_alpha
+    else
+      @distillery.scotches
+    end
   end
 
   def new
-    scotch = Scotch.new
+    @scotch = Scotch.new
     @distillery = Distillery.find(params[:distillery_id])
   end
 
   def create
     distillery = Distillery.find(params[:distillery_id])
-    scotch = distillery.scotches.create({
-      name: params[:scotch][:name],
-      single_malt: params[:scotch][:single_malt],
-      year: params[:scotch][:year]
-    })
-    scotch.save
+    @scotch = distillery.scotches.create({
+                  name: params[:name],
+                  single_malt: params[:single_malt],
+                  year: params[:year]
+                })
     redirect_to "/distilleries/#{distillery.id}/scotches"
-    # render :action => :index
-  end
-
-  def after_action
-    @scotches = @distillery.scotches.all.order(name: :asc)
-    render :action => :index
   end
 end
