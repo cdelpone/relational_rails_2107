5c8f0df4619766bdc47f7fe37f14736036017c35
Christina Delpone <cdelpone@gmail.com>
2021-09-01 16:22:35 -0600

    Begin model testing

--- spec/models/distillery_spec.rb
+++ spec/models/distillery_spec.rb
@@ -62,7 +62,7 @@ describe Distillery, type: :model do
   describe 'validations' do
     it { should validate_presence_of :name }
     it { should validate_presence_of :established }
-    it { should validate_presence_of :scotland_location }
+    it { should validate_inclusion_of :scotland_location, :in => [true, false] }
   end
 
   describe 'class methods' do
